912e931539b6ef1a7ba6e921804638a887dcb347
Youngjun <dudwns9426@naver.com>
2023-12-06 15:39:30 +0900

    first commit

---
+++ src/main/java/com/rmsoft/RmSoftApplication.java
@@ -0,0 +1,13 @@
+package com.rmsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmSoftApplication.class, args);
+	}
+
+}
